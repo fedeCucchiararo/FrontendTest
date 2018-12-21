aa7e3665d29bc6457edbbcdd669bcaa7984d0b5b
Fede <fedegomezcucchiararo@gmail.com>
2018-12-21 11:38:18 +0100

    change main.js

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
 /** get form element */
 let form = document.getElementById("search-form");
-
+/** get input element */
+let search_input = document.getElementById("search-input");
 /** get element into which results will be rendered */
 let searchResult = document.getElementById("search-result");
 
-form.addEventListener("submit", event => {
-  event.preventDefault();
+form.addEventListener("submit", searchHandler);
 
+function searchHandler(event) {
+  event.preventDefault();
   // remove previous results
   DOMService.cleanSearchResults();
-
   // get the input value to perform the fetch
-  let search_query = document.getElementById("search-input").value;
+  let query = search_input.value;
 
   githubService
-    .searchUser(search_query)
+    .searchUser(query)
     .then(async ({ repos_url, avatar_url, login, name, bio }) => {
       let userRepos = await githubService.getUserRepos(repos_url);
       DOMService.renderRepos({ userRepos, avatar_url, login, name, bio });
     })
     .catch(error => DOMService.displayError(error));
-});
-
-// user => ({ repos_url, avatar_url, login, name, bio } = user)
+}
